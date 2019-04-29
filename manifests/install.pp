a976a258b067899881a55cf4020dedb2b6122a0f
Jon C. Meyer <jmeyer@ucsd.edu>
2019-04-29 00:07:28 -0700

    Modify most of module to use epp() instead of template(). Modernize args to Puppet 4 standards

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,102 +1,104 @@
 # == Class: znapzend::install
 #
 class znapzend::install {
-  if $::znapzend::package_manage {
-    package { $::znapzend::package_name :
-      ensure   => $::znapzend::package_ensure,
-      name     => $::znapzend::package_name,
+  file {
+    $znapzend::service_pid_dir:
+      ensure  => 'directory',
+      owner   => $znapzend::user,
+      group   => $znapzend::group,
+      mode    => '0644',
+      recurse => true,
+      before  => File[$znapzend::service_log_dir],;
+    $znapzend::service_log_dir:
+      ensure => 'directory',
+      owner  => $znapzend::user,
+      group  => $znapzend::group,
+      mode   => '0755',
+      before => File[$znapzend::service_conf_dir],;
+    $znapzend::service_conf_dir:
+      ensure => 'directory',
+      owner  => $znapzend::user,
+      group  => $znapzend::group,
+      mode   => '0755',;
+  }
+
+  if $znapzend::package_manage {
+    package { $znapzend::package_name :
+      ensure => $znapzend::package_ensure,
+      name   => $znapzend::package_name,
     }
   }
 
   # init script(s)
   case $::osfamily {
     'FreeBSD': {
-        file { "/usr/local/etc/rc.d/$::znapzend::service_name":
-          owner    => 'root',
-          group    => 'wheel',
-          mode     => '0755',
-          content  => template('znapzend/znapzend_init_freebsd.erb'),
-          notify   => Service[$znapzend::service_name],
+        file { "/usr/local/etc/rc.d/${znapzend::service_name}":
+          ensure  => 'file',
+          owner   => 'root',
+          group   => 'wheel',
+          mode    => '0755',
+          content => epp('znapzend/znapzend_init_freebsd.epp'),
+          notify  => Service[$znapzend::service_name],
         }
-    }
-    'RedHat': {
-
+    } 'RedHat': {
         file { '/lib/systemd/system/znapzend.service':
-          owner     => 'root',
-          group     => 'root',
-          mode      => '0644',
-          content   => template('znapzend/znapzend_init_centos.erb'),
-          notify   => Service[$znapzend::service_name],
+          ensure  => 'file',
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => epp('znapzend/znapzend_init_redhat.epp'),
+          notify  => Service[$znapzend::service_name],
         }
-    }
-    'Solaris': {
-        file { "/lib/svc/method/$::znapzend::service_name":
-          owner    => 'root',
-          group    => 'bin',
-          mode     => '0555',
-          content  => template('znapzend/znapzend_init_solaris.erb'),
-          notify   => Service[$znapzend::service_name],
-        }
-        file { "/var/svc/manifest/system/filesystem/${::znapzend::service_name}.xml":
-          owner    => 'root',
-          group    => 'bin',
-          mode     => '0555',
-          content  => template('znapzend/znapzend.xml.erb'),
-          notify   => Exec['reload-manifest'],
+    } 'Solaris': {
+        file {
+          "/lib/svc/method/${znapzend::service_name}":
+            ensure  => 'file',
+            owner   => 'root',
+            group   => 'bin',
+            mode    => '0555',
+            content => epp('znapzend/znapzend_init_solaris.epp'),
+            notify  => Service[$znapzend::service_name],;
+          "/var/svc/manifest/system/filesystem/${znapzend::service_name}.xml":
+            ensure  => 'file',
+            owner   => 'root',
+            group   => 'bin',
+            mode    => '0555',
+            content => epp('znapzend/znapzend.xml.epp'),
+            notify  => Exec['reload-manifest'],;
         }
         exec { 'reload-manifest':
-          command      => "/usr/sbin/svccfg import /var/svc/manifest/system/filesystem/${::znapzend::service_name}.xml",
-          refreshonly => true, 
+          refreshonly => true,
+          command     => join([
+            '/usr/sbin/svccfg import',
+            "/var/svc/manifest/system/filesystem/${znapzend::service_name}.xml",
+          ], ' '),
         }
+    } default: {
+      # NOOP
     }
   }
 
   # manage sudo
-  if $::znapzend::manage_sudo {
-    file { "$::znapzend::sudo_d_path/znapzend":
-          owner  => 'root',
-          mode   => '0440',
-          content => template('znapzend/znapzend_sudo.erb'),
-        }
+  if $znapzend::manage_sudo {
+    file { "${znapzend::sudo_d_path}/znapzend":
+      owner   => 'root',
+      mode    => '0440',
+      content => epp('znapzend/znapzend_sudo.epp'),
+    }
   }
 
-  # add non-root user/group if specified 
-  if $::znapzend::user != 'root' {
-    if $::znapzend::manage_user {
-      group { $::znapzend::group:
-        ensure   => 'present',
-      } ->
-      user { $::znapzend::user:
-        ensure   => 'present',
-        comment  => 'znapzend backup user',
-        shell    => $::znapzend::user_shell,
-        home     => $::znapzend::user_home,
-      } 
+  # add non-root user/group if specified
+  if $znapzend::user != 'root' {
+    if $znapzend::manage_user {
+      group { $znapzend::group:
+        ensure => 'present',
+      }
+      -> user { $znapzend::user:
+        ensure  => 'present',
+        comment => 'znapzend backup user',
+        shell   => $znapzend::user_shell,
+        home    => $znapzend::user_home,
+      }
     }
-  } 
-  # pid dir
-  file { $::znapzend::service_pid_dir:
-    ensure   => 'directory',
-    owner    => $::znapzend::user,
-    group    => $::znapzend::group,
-    mode     => '0644',
-    recurse  => true,
-  } ->
-  # log dir
-  file { $::znapzend::service_log_dir:
-    ensure   => 'directory',
-    owner    => $::znapzend::user,
-    group    => $::znapzend::group,
-    mode     => '0755',
-  } ->
-  # config dir
-  file { $::znapzend::service_conf_dir:
-    ensure   => 'directory',
-    owner    => $::znapzend::user,
-    group    => $::znapzend::group,
-    mode     => '0755',
-  } 
-  
-
-
+  }
 }
--- spec/classes/znapzend_spec.rb
+++ spec/classes/znapzend_spec.rb
@@ -1,209 +1,229 @@
-require 'spec_helper'
+require 'spec_helper' # frozen_string_literal: true
 
-describe 'znapzend', :type => :class do
-
-  ['CentOS', 'RedHat', 'FreeBSD', 'Solaris'].each do |system|
+# rubocop:disable Metrics/BlockLength
+describe 'znapzend', type: :class do
+  %w[CentOS RedHat FreeBSD Solaris].each do |system|
     context "when on system #{system}" do
       if system == 'CentOS'
-        let(:facts) do
-          {
-            :osfamily        => 'RedHat',
-            :operatingsystem => system,
-          }
+        let :facts do
+          { osfamily: 'RedHat', operatingsystem: system }
         end
       else
-        let(:facts) do
+        let :facts do
+          { osfamily: system, operatingsystem: system }
+        end
+      end
+
+      it do should contain_class('znapzend::install') end
+      it do should contain_class('znapzend::service') end
+      it do should contain_class('znapzend::plans') end
+
+      describe 'znapzend::install' do
+        if system == 'Solaris'
+          let :params do
+            { package_manage: false, package_name: 'znapzend' }
+          end
+          it do should_not contain_package('znapzend') end
+        else
+          let :params do
+            {
+              package_ensure: 'present',
+              package_name: 'znapzend',
+              package_manage: true
+            }
+          end
+          it do should contain_package('znapzend').with_ensure('present') end
+
+          context 'should allow package ensure to be overridden' do
+            let :params do
+              { package_ensure: 'latest', package_name: 'znoobar' }
+            end
+            it do should contain_package('znoobar').with_ensure('latest') end
+          end
+
+          context 'should allow the package name to be overridden' do
+            let :params do
+              { package_name: 'foo' }
+            end
+            it do should contain_package('foo') end
+          end
+
+          context 'should allow the package to be unmanaged' do
+            let :params do
+              { package_name: 'znapzend', package_manage: false }
+            end
+            it do should_not contain_package('znapzend') end
+          end
+        end
+
+        describe 'init.d script' do
+          let :params do
+            { service_name: 'znapzend' }
+          end
+
+          if system == 'FreeBSD'
+            context 'should create init.d script on FreeBSD' do
+              it do
+                should contain_file('/usr/local/etc/rc.d/znapzend').with(
+                  ensure: 'file',
+                  owner: 'root',
+                  group: 'wheel',
+                  mode: '0755',
+                  content: %r{\ncommand="/opt/znapzend/bin/znapzend"}
+                )
+              end
+            end
+          end
+
+          if system == 'RedHat'
+            context 'should create init.d script on RedHat osFamily' do
+              it do
+                should contain_file(
+                  '/lib/systemd/system/znapzend.service'
+                ).with(
+                  ensure: 'file',
+                  owner: 'root',
+                  group: 'root',
+                  mode: '0644',
+                  content: %r{\nExecStart=/opt/znapzend/bin/znapzend}
+                )
+              end
+            end
+          end
+
+          if system == 'Solaris'
+            context 'should create init.d script on Solaris' do
+              it do should contain_file('/lib/svc/method/znapzend') end
+              it do
+                should contain_file(
+                  '/var/svc/manifest/system/filesystem/znapzend.xml'
+                )
+              end
+            end
+          end
+        end
+
+        describe 'manage sudo' do
+          context 'should create znapzend sudo file' do
+            let :params do
+              { sudo_d_path: '/usr/local/etc/sudoers.d', manage_sudo: true }
+            end
+            it do should contain_file('/usr/local/etc/sudoers.d/znapzend') end
+          end
+
+          context 'should all for sudo to be unmanaged' do
+            let :params do
+              { sudo_d_path: '/usr/local/etc/sudoers.d', manage_sudo: false }
+            end
+            it do
+              should_not contain_file('/usr/local/etc/sudoers.d/znapzend')
+            end
+          end
+        end
+
+        describe 'should create user and group' do
+          let :params do
+            {
+              manage_user: true,
+              user: 'znapzend',
+              group: 'znapzend',
+              user_shell: '/bin/bash',
+              user_home: '/home/znapzend',
+              service_pid_dir: '/var/run/znapzend',
+              service_log_dir: '/var/log/znapzend',
+              service_conf_dir: '/usr/local/etc/znapzend'
+            }
+          end
+
+          it do
+            should contain_group('znapzend').with_ensure('present')
+          end
+          it do
+            should contain_user('znapzend').with(
+              ensure: 'present',
+              shell: '/bin/bash',
+              home: '/home/znapzend'
+            )
+          end
+
+          context 'should create pid dir' do
+            it do
+              should contain_file('/var/run/znapzend').with(
+                ensure: 'directory',
+                owner: 'znapzend',
+                group: 'znapzend',
+                mode: '0644',
+                recurse: true
+              )
+            end
+          end
+
+          context 'should create log dir' do
+            it do
+              should contain_file('/var/log/znapzend').with(
+                ensure: 'directory',
+                owner: 'znapzend',
+                group: 'znapzend',
+                mode: '0755'
+              )
+            end
+          end
+
+          context 'should create config dir' do
+            it do
+              should contain_file('/usr/local/etc/znapzend').with(
+                ensure: 'directory',
+                owner: 'znapzend',
+                group: 'znapzend',
+                mode: '0755'
+              )
+            end
+          end
+        end
+      end
+
+      describe 'znapzend::service' do
+        let :params do
+          { service_name: 'znapzend' }
+        end
+        describe 'with defaults' do
+          it do
+            should contain_service('znapzend').with(
+              hasstatus: true,
+              ensure: 'running',
+              enable: true
+            )
+          end
+        end
+      end
+
+      # test multiple plans can be passed
+      describe 'znapzend::plans' do
+        let :params do
           {
-            :osfamily        => system,
-            :operatingsystem => system,
+            service_conf_dir: '/usr/local/etc/znapzend',
+            plans: {
+              tank_foobar: { config_src: 'tank/foobar' },
+              backup_tank: { config_src: 'backup/tank' }
+            }
           }
         end
-      end
-
-    it { should contain_class('znapzend::install') }
-    it { should contain_class('znapzend::service') }
-    it { should contain_class('znapzend::plans') }
-
-    describe 'znapzend::install' do
-      if system == "Solaris"
-        let(:params) {{
-          :package_manage => false,
-          :package_name   => 'znapzend',
-        }}
-        it { should_not contain_package('znapzend') }
-      else
-        let(:params) {{
-          :package_ensure => 'present',
-          :package_name   => 'znapzend',
-          :package_manage => true,
-        }}
-        it { should contain_package('znapzend').with_ensure('present') }
-
-        context 'should allow package ensure to be overridden' do
-          let(:params) {{
-            :package_ensure => 'latest',
-            :package_name   => 'znoobar',
-          }}
-          it { should contain_package('znoobar').with_ensure('latest') }
+        it do
+          should contain_file('/usr/local/etc/znapzend/tank_foobar').with(
+            owner: 'znapzend',
+            group: 'znapzend',
+            notify: 'Exec[load_tank_foobar]'
+          )
         end
-
-        context 'should allow the package name to be overridden' do
-          let(:params) {{
-            :package_name => 'foo',
-          }}
-          it { should contain_package('foo') }
+        it do
+          should contain_file('/usr/local/etc/znapzend/backup_tank').with(
+            owner: 'znapzend',
+            group: 'znapzend',
+            notify: 'Exec[load_backup_tank]'
+          )
         end
-
-        context 'should allow the package to be unmanaged' do
-          let(:params) {{
-            :package_name   => 'znapzend',
-            :package_manage => false,
-          }}
-          it { should_not contain_package('znapzend') }
-        end
-
       end
- 
-      describe 'init.d script' do
-
-        let(:params) {{
-          :service_name => 'znapzend',
-        }}      
-
-        if system == "FreeBSD"
-          context 'should create init.d script on FreeBSD' do
-            it { should contain_file('/usr/local/etc/rc.d/znapzend') }
-          end
-        end
-   
-        if system == "RedHat"
-          context 'should create init.d script on RedHat osFamily' do
-            it { should contain_file('/lib/systemd/system/znapzend.service') }
-          end
-        end
-   
-        if system == "Solaris"
-          context 'should create init.d script on Solaris' do
-            it { should contain_file('/lib/svc/method/znapzend') }
-            it { should contain_file('/var/svc/manifest/system/filesystem/znapzend.xml') }
-          end
-        end
-   
-      end
-   
-      describe 'manage sudo' do
-        context 'should create znapzend sudo file' do
-           let(:params) {{
-             :sudo_d_path   => '/usr/local/etc/sudoers.d',
-             :manage_sudo   => true,
-           }}
-           it { should contain_file('/usr/local/etc/sudoers.d/znapzend') }
-        end  
-
-        context 'should all for sudo to be unmanaged' do
-           let(:params) {{
-             :sudo_d_path   => '/usr/local/etc/sudoers.d',
-             :manage_sudo   => false,
-           }}
-           it { should_not contain_file('/usr/local/etc/sudoers.d/znapzend') }
-        end  
-      end
-
-      describe 'should create user and group' do
-         let(:params) {{
-           :manage_user      => true,
-           :user             => 'znapzend',
-           :group            => 'znapzend',
-           :user_shell       => '/bin/bash',
-           :user_home        => '/home/znapzend', 
-           :service_pid_dir  => '/var/run/znapzend',
-           :service_log_dir  => '/var/log/znapzend',
-           :service_conf_dir => '/usr/local/etc/znapzend',
-         }}
-
-         it { should contain_group('znapzend').with({
-           'ensure'  => 'present',
-         })}
-         it { should contain_user('znapzend').with({
-           'ensure'  => 'present',
-           'shell'    => '/bin/bash',
-           'home'     => '/home/znapzend', 
-         })}
-
-         context 'should create pid dir' do
-           it { should contain_file('/var/run/znapzend').with({
-             'ensure' 	 => 'directory',
-             'owner'     => 'znapzend',
-             'group'     => 'znapzend',
-             'mode'      => '0644',
-             'recurse'   => true,
-           })}
-         end
-
-         context 'should create log dir' do
-           it { should contain_file('/var/log/znapzend').with({
-             'ensure' 	 => 'directory',
-             'owner'     => 'znapzend',
-             'group'     => 'znapzend',
-             'mode'      => '0755',
-           })}
-         end
-
-         context 'should create config dir' do
-           it { should contain_file('/usr/local/etc/znapzend').with({
-             'ensure' 	 => 'directory',
-             'owner'     => 'znapzend',
-             'group'     => 'znapzend',
-             'mode'      => '0755',
-           })}
-         end
-      end
-
-    end # describe znapzend::install
-
-    describe 'znapzend::service' do
-
-      let(:params) {{
-        :service_name => 'znapzend',
-      }}      
-      describe 'with defaults' do
-        it { should contain_service('znapzend').with({
-          :hasstatus => true,
-          :ensure    => 'running',
-          :enable    => true,
-        })}
-      end
-
-      
-    end # describe znapzend::service
-
-    # test multiple plans can be passed
-    describe 'znapzend::plans' do 
-      let :params do {
-        :service_conf_dir => '/usr/local/etc/znapzend',
-        :plans => {
-          'tank_foobar' => {
-            'config_src'     => 'tank/foobar',
-          },
-          'backup_tank' => {
-            'config_src'     => 'backup/tank',
-          },
-        },
-      }
-      end
-      it { should contain_file('/usr/local/etc/znapzend/tank_foobar').with({
-        :owner  => "znapzend",
-        :group  => "znapzend",
-        :notify => "Exec[load_tank_foobar]",
-      })}
-      it { should contain_file('/usr/local/etc/znapzend/backup_tank').with({
-        :owner  => "znapzend",
-        :group  => "znapzend",
-        :notify => "Exec[load_backup_tank]",
-      })}
-
-    end # end znapzend::plans
-
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
